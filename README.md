2c096ca9daafcdf66c03fbd6e4fc558feb6187c2
Anastaciii <49093339+Anastaciii@users.noreply.github.com>
2019-05-18 00:46:45 +0100

    Structure
    Description of changes in readme

--- README.md
+++ README.md
@@ -3,5 +3,14 @@
 
 1. Create an https://www.are.na/ account
 2. Follow the collaborative are.na channel = https://www.are.na/annabel-lake/callback-cats
-3. Add images, videos, notes, inspo websites etc. so we have a bank for all visual and other resources 
+3. Add images, videos, notes, inspo websites etc. so we have a bank for all visual and other resources
 
+AG
+4. Created the grid, which includes main page / four sport sections an da placeholder for the contact section - need to fix the layout and make it responsive
+5. Created side button scrollbar
+6. Add styles (those in CSS are for visual purposes only)
+7. Add contacts and contact form
+8. Add About page
+9. Add java features
+10. Populate with images, text, titles (running / sailing / Emma & Alex - please remind what sport you chose), we can also add icons
+11. Think about how we want to present it
---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+function changeDot() {
+    const scrollValue = $(window).scrollTop();
+    const heightSec2 = $('.section2').offset().top;
+    const heightSec3 = $('.section3').offset().top;
+    const heightSec4 = $('.section4').offset().top;
+
+    if (scrollValue < heightSec2) {
+        $('nav li').not('.dot1').removeClass('active');
+        $('.dot1').addClass('active');
+    } else if (scrollValue < heightSec3) {
+        $('nav li').not('.dot2').removeClass('active');
+        $('.dot2').addClass('active');
+    } else if (scrollValue < heightSec4) {
+        $('nav li').not('.dot3').removeClass('active');
+        $('.dot3').addClass('active');
+    } else {
+        $('nav li').not('.dot4').removeClass('active');
+        $('.dot4').addClass('active');
+    }
+}
+
+$(window).on("scroll", changeDot)
+
+$('nav li').on('click', function () {
+    const goToSection = '.s' + $(this).attr('id');
+    $('body, html').animate({
+        scrollTop: $(goToSection).offset().top + 1
+    })
+})
